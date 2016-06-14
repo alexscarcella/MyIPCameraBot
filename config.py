659bd102bb32898d8e59fcd0b90f80a202e46735
scarcella.alex@gmail.com <alx73GH>
2016-06-14 20:17:45 +0200

    Introdotto un file di configurazione esterno

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python2.7
-TELEGRAM_BOT_TOKEN="fffffffff" #inserite qui il Token segreto del BOT Telegram
-TELEGRAM_USERS_ID="0000000"
+TELEGRAM_BOT_TOKEN="fffffffff" # inserite qui il Token segreto del BOT Telegram
+TELEGRAM_USERS_ID="0000000" # inserite qui l'ID dell'utente Telegram a cui volete spedire le immagini
